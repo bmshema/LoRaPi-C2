dd7fc5de1f69e867f727bc4338c1eb262899e667
bmshema <92175742+bmshema@users.noreply.github.com>
2023-08-06 14:45:38 -0400

    Add while loop setup from menu selection

--- sender.py
+++ sender.py
@@ -4,6 +4,7 @@ import time
 import termios
 import tty
 import sys
+import select
 
 # Termios setup
 old_settings = termios.tcgetattr(sys.stdin)
@@ -17,6 +18,17 @@ try:
     # Initial prompts
     utils.header()
     print("Press \033[1;32mEsc\033[0m to exit || Press \033[1;32ms\033[0m to send a command\n")
+
+    while True:
+        if select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], []):
+            c = sys.stdin.read(1)
+
+        # Esc Key detection
+        if c == '\x1b':
+            break
+        # s key detection
+        if c == '/x73':
+            pass
 
 except:
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
