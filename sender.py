4d59e7e9cadf3454b774ecabaf4f63ddfde57286
bmshema <92175742+bmshema@users.noreply.github.com>
2023-08-06 15:09:12 -0400

    Add send_command function to send data

--- sender.py
+++ sender.py
@@ -13,6 +13,28 @@ tty.setcbreak(sys.stdin.fileno())
 # Create sx126x node
 node = sx126x(serial_num = "/dev/ttyS0",freq=868,addr=0,power=22,rssi=True,air_speed=2400,relay=False)
 
+def send_command():
+    print("Input command in the following format:\n")
+    print("\033[1;32m0,868,shutdown now\033[0m")
+    print("This will send the command to a device with address 0, on frequency 868, to shutdown now\n")
+
+    while True:
+        rec = sys.stdin.read(1)
+        if rec != None:
+            if rec == '\x0a':
+                break
+            get_rec += rec
+            sys.stdout.write(rec)
+            sys.stdout.flush()
+    
+    get_t = get_rec.split(',')
+    offset_freq = int(get_t[1])-(850 if int(get_t[1])>850 else 410)
+
+    data = bytes([int(get_t[0])>>8]) + bytes([int(get_t[0])&0xff]) + bytes([offset_freq]) + bytes([node.addr>>8]) + bytes([node.addr&0xff]) + bytes([node.offset_freq]) + get_t[2].encode()
+
+    node.send(data)
+    print("\n\033[1;32mCommand sent!\033[0m\n")
+
 try:
     time.sleep(1)
     # Initial prompts
@@ -29,6 +51,10 @@ try:
         # s key detection
         if c == '/x73':
             pass
+
+        sys.stdout.flush()
+    
+    node.receive()
 
 except:
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
