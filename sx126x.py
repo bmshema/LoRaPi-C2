cfe78de3ff0233175cc972faa15ca9b32e97f627
bmshema <92175742+bmshema@users.noreply.github.com>
2023-08-06 17:31:48 -0400

    Modify receive funtion to return incoming data

--- sx126x.py
+++ sx126x.py
@@ -3,6 +3,7 @@
 import RPi.GPIO as GPIO
 import serial
 import time
+import re
 
 class sx126x:
 
@@ -256,17 +257,7 @@ class sx126x:
             time.sleep(0.5)
             r_buff = self.ser.read(self.ser.inWaiting())
 
-            print("receive message from node address with frequence\033[1;32m %d,%d.125MHz\033[0m"%((r_buff[0]<<8)+r_buff[1],r_buff[2]+self.start_freq),end='\r\n',flush = True)
-            print("message is "+str(r_buff[3:-1]),end='\r\n')
-            
-            # print the rssi
-            if self.rssi:
-                # print('\x1b[3A',end='\r')
-                print("the packet rssi value: -{0}dBm".format(256-r_buff[-1:][0]))
-                self.get_channel_rssi()
-            else:
-                pass
-                #print('\x1b[2A',end='\r')
+            return (r_buff[3:-1])
 
     def get_channel_rssi(self):
         GPIO.output(self.M1,GPIO.LOW)
