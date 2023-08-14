90eb93db28d9a763746ac3905a2c6f0b97e43dca
bmshema <92175742+bmshema@users.noreply.github.com>
2023-08-14 15:53:25 -0400

    Add logging to preserve received commands output

--- receiver.py
+++ receiver.py
@@ -2,6 +2,7 @@
 import subprocess
 from sx126x import sx126x
 import time
+import datetime
 
 # Create sx126x node
 node = sx126x(serial_num = "/dev/ttyS0",freq=915,addr=1,power=22,rssi=True,air_speed=2400,relay=False)
@@ -15,21 +16,28 @@ try:
          Receiver Node\n")
 
     while True:
+        log = open("receiver_node.log", "a")
+        clock = datetime.now()
+
         incoming_data = node.receive()
         if incoming_data:
             command = incoming_data.decode().split(',')
             
-            print(f"Command Received: {command} @ -{node.get_rssi()} dBm")
-            
+            print(f" {clock} Command Received: {command} @ -{node.get_rssi()} dBm")
+            log.write(f"{clock} Command Received: {command} @ -{node.get_rssi()} dBm")
             try:
                 output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
                 print("Command output:\n", output.decode())
+                log.write("Command output:\n" + output.decode())
                 
             except subprocess.CalledProcessError as e:
                 print("Error executing command:", e.output.decode())
+                log.write("Error executing command:", e.output.decode())
 
         time.sleep(1)
+        log.close()
 
 except KeyboardInterrupt:
     node.close()
+    log.close()
     exit()
